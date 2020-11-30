79c04d20298e5cfe153b1e37890f245d47a356d9
Morgane Perenzoni <mperenzoni@gmail.com>
2020-11-30 12:16:24 +0100

    first notion in react

--- src/App.js
+++ src/App.js
@@ -34,10 +34,15 @@ class App extends Component {
     team.membre1.age +=1
     this.setState({ team })
   }
-  handleChange = event => {
+  handleChange = (event, id) => {
     const team = {  ...this.state.team }
     const nom = event.target.value
-    team.membre1.nom = nom
+    team[id].nom = nom
+    this.setState({ team })
+  }
+  cacherNom = id => {
+    const team = {  ...this.state.team }
+    team[id].nom ='X'
     this.setState({ team })
   }
   handleShowDescription = () => {
@@ -48,36 +53,43 @@ class App extends Component {
   render () {
     const { titre } = this.props
     const { team, isShow } = this.state
+
+    let description = null
+
+    if ( isShow ) {
+      description =  (
+      <strong> Je suis une baby dev </strong>
+      )
+    }
+
+    const liste = Object.keys( team )
+      .map( membre => (
+        <Membre
+        key={membre}
+        handleChange={event => this.handleChange(event, membre)}
+        cacherNom={() => this.cacherNom(membre)}
+        nom={team[membre].nom}
+        age={team[membre].age} />
+      
+      )
+      )
+
     return (
       <div className='App'>
         <h1> {titre} </h1>
-        <input value={team.membre1.nom} onChange = {this.handleChange} 
-        type='text'/>
-      <Membre 
-        nom={team.membre1.nom}
-        age={team.membre1.age} />
-      <Membre 
-        nom={team.membre2.nom}
-        age={team.membre2.age}/>
-      <Membre 
-        nom={team.membre3.nom}
-        age={team.membre3.age}/>
-      <Membre 
+        { liste }
+      {/*<Membre 
         nom={team.membre4.nom}
-        age={team.membre4.age}>
-      
-      { 
-        isShow ? <strong> Je suis une baby dev </strong> : null
-      }
+        age={team.membre4.age}>     
+      { description }
     <button onClick={this.handleShowDescription}>
       {
         isShow ? 'Cacher' : 'Montrer'
-      } 
-      
+      }       
       </button>
-      </Membre>
+    </Membre>
       <Button 
-        vieillir={this.handleClick}/>
+        vieillir={this.handleClick}/>*/}
       </div>
     )
   }
--- src/components/Membre.js
+++ src/components/Membre.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react'
 
-const Membre = ({ nom, children, age }) => {
+const Membre = ({ nom, children, age, cacherNom, handleChange }) => {
     return (
         <Fragment>
             <h2> {nom} : {age} ans </h2>
             <p>{children}</p>
+            <input value={nom} onChange = {handleChange} type='text'/>
+            <button onClick={cacherNom}> X </button>
         </Fragment>
     )
 
